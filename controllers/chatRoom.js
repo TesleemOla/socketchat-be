8a7035b6abb0d35bbb46a04da48929e771843f1d
Tesleemikook <tesleem@ikook.co.uk>
2025-03-16 12:10:31 +0100

    removed all files added with wrong username

--- controllers/chatRoom.js
+++
@@ -1,7 +0,0 @@
-export default {
-    initiate: async (req, res) => { },
-    postMessage: async (req, res) => { },
-    getRecentConversation: async (req, res) => { },
-    getConversationByRoomId: async (req, res) => {},
-    markConversationAsReadByRoomId: async (req, res) => {},
-}
